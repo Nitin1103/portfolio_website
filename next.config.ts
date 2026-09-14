a83a5eb947dda1d6c753fb6371c79c57b20c3c46
Nitin <nitinisatwork@gmail.com>
2026-09-14 21:17:25 +0530

    Configure automated GitHub Pages deployment via GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isGithubActions ? "/portfolio_website" : "",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
